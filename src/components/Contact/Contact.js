5f355848553676b69139f7844a1e7204fd726b37
Christine <calyston.14@gmail.com>
2021-04-12 09:52:47 +0100

    feat: formspree added to contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,10 +3,10 @@
 import React, { useState } from 'react'
 
 const Contact = () => {
-  const [ name, setName ] = useState('')
-  const [ email, setEmail ] = useState('')
-  const [ subject, setSubject ] = useState('')
-  const [ message, setMessage ] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
 
   const onNameChange = e => {
     setName(e.target.value)
@@ -24,36 +24,36 @@ const Contact = () => {
     setMessage(e.target.value)
   }
 
-  return(
+  return (
     <main className="contact-page">
-    <h1>Contact Us</h1>
-    <div id="contactText">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      <h1>Contact Us</h1>
+      <div id="contactText">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua.</p>
-    </div>
-    <section className="contact-form">
-      <form>
-        <div className="name-form">
-          <label htmlFor="name"><span>*</span>Full Name</label>
-          <input value={name} onChange={onNameChange} id="name" type="text" name="name" required/>
-        </div>
-        <div className="email-form">
-          <label htmlFor="email"><span>*</span>Email</label>
-          <input value={email} onChange={onEmailChange} id="email" type="email" name="email" required/>
-        </div>
-        <div className="subject-form">
-          <label htmlFor="subject"><span>*</span>Subject</label>
-          <input value={subject} onChange={onSubjectChange} id="subject" type="text" name="subject" required/>
-        </div>
-        <div className="message-form">
-          <label htmlFor="message"><span>*</span>Message</label>
-          <textarea value={message} onChange={onMessageChange} name="message" id="message" cols="30" rows="10" placeholder="Write a message..."
-            required></textarea>
-        </div>
-        <button className='add-to-bag' type="submit">Submit</button>
-      </form>
-    </section>
-  </main>
+      </div>
+      <section className="contact-form">
+        <form action="https://formspree.io/f/mnqopaer" method="POST">
+          <div className="name-form">
+            <label htmlFor="name"><span>*</span>Full Name</label>
+            <input value={name} onChange={onNameChange} id="name" type="text" name="name" required />
+          </div>
+          <div className="email-form">
+            <label htmlFor="email"><span>*</span>Email</label>
+            <input value={email} onChange={onEmailChange} id="email" type="email" name="email" required />
+          </div>
+          <div className="subject-form">
+            <label htmlFor="subject"><span>*</span>Subject</label>
+            <input value={subject} onChange={onSubjectChange} id="subject" type="text" name="subject" required />
+          </div>
+          <div className="message-form">
+            <label htmlFor="message"><span>*</span>Message</label>
+            <textarea value={message} onChange={onMessageChange} name="message" id="message" cols="30" rows="10" placeholder="Write a message..."
+              required></textarea>
+          </div>
+          <button className='add-to-bag' type="submit">Submit</button>
+        </form>
+      </section>
+    </main>
   )
 }
 
