315767e3a3f8212eb3d13e40b32167dde53aa7a0
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2021-05-11 12:41:53 -0500

    feat: faq link tree

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { steffectContext } from '../../Context'
 import loadingGif from '../../assets/icons/loading-spinner.gif'
+import { Link } from 'react-router-dom'
 
 const Faq = () => {
   const { pages, loading} = useContext(steffectContext)
@@ -9,6 +10,11 @@ const Faq = () => {
 
   return(
     <main className="faq-page">
+      <div className="linktree">
+        <Link to='/'>Home</Link>
+        <p>{'>'}</p>
+        <span>Frequently Asked Questions</span>
+      </div>
     <h1>Frequently Asked Questions</h1>
     <div id="faqText">
       {loading ? <img src={loadingGif} alt='loading...'/> : faq && faq.faqText.split('\n').map((line, index) => {
